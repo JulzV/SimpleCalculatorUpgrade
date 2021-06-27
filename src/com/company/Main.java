f56afd65ef8eb8c9d573145ba4f3bdf54f9e0884
JulzV <volkova.julija@inbox.lv>
2021-06-27 19:41:06 +0300

    Min & Max function added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,26 @@ public class Main {
     public static void main(String[] args) {
 	    double firstValue;
 	    double secondValue;
-	    double answer;
-
 
 //  Initialise variables
         firstValue = 22;
         secondValue = 7;
-        answer = 0;
 
+//  Entering one operator: +, -, /, *
 	    String operator = "*";
 
-        System.out.println(doArithmetic(firstValue, secondValue, operator));
+//  Entering operator Min or Max
+	    String operatorMinMax = "Min";
+
+//  Print out results
+        System.out.println("Arithmetic result: " + doArithmetic(firstValue, secondValue, operator));
+
+        System.out.println("Min or Max result: " + operatorMinMax + " = " + minMaxFunction(firstValue, secondValue, operatorMinMax));
+
 
     }
 
+//  Methode 1: calculation
     public static double doArithmetic(double firstNumber, double secondNumber, String operator){
         double answer = 0;
         switch (operator) {
@@ -41,8 +47,21 @@ public class Main {
         }
         return answer;
 
-
-        // create a method that checks two numbers and uses a switch to return max, min
-
     }
+//  HOME TASK: create a method that checks two numbers and uses a switch to return max, min
+
+    //  Methode 2: finding Min or Max  values
+    public static double minMaxFunction (double minMax1, double minMax2, String operator){
+        double answer = 0;
+        switch (operator){
+            case "Min":
+                answer = Math.min(minMax1, minMax2);
+                return answer;
+            case "Max":
+                answer = Math.max(minMax1, minMax2);
+                return answer;
+        }
+    return answer;
+    }
+
 }
